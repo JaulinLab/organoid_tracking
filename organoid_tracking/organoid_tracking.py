7dc45f7c129f805a26461a9341b1adab6563ddcb
Gustave Ronteix <gustave.ronteix@hotmail.fr>
2022-08-31 15:56:02 +0200

    prop extraction from masks

--- organoid_tracking/organoid_tracking.py
+++ organoid_tracking/organoid_tracking.py
@@ -4,14 +4,16 @@ import pandas as pd
 import skimage.measure
 
 
-def get_mask_properties(label_image, properties=("centroid", "perimeter", "area")):
+def get_mask_properties(mask_image, properties=("centroid", "perimeter", "area")):
+
+    label_image = skimage.measure.label(mask_image)
 
     mask_properties = skimage.measure.regionprops_table(
         label_image, properties=properties
     )
 
     mask_properties = pd.DataFrame(mask_properties).rename(
-        {"centroid-0": "x", "centroid-1": "y"}, axis="columns"
+        {"centroid-0": "y", "centroid-1": "x"}, axis="columns"
     )
 
     return mask_properties
